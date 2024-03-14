754942d8120a8b2b2f9a2acd410222f5f662230c
Yaze Li <163340829+Arthurliyaze@users.noreply.github.com>
2024-03-14 12:49:03 -0700

    Add files via upload

---
+++ DQN_detection/pstv3/ml_sig.m
@@ -0,0 +1,28 @@
+function f = ml_sig(t,k)
+% Syntax: f = ml_sig(t,k)
+%4:40 PM 15/08/97
+% defines modulation signal for lmod control
+global lmod_sig n_lmod
+f=0; %dummy variable
+% lmod_sig(:,k)=zeros(n_lmod,1);
+%if n_lmod~=0
+%    lmod_sig(:,k)=zeros(n_lmod,1);
+    %lmod_sig(:,k) = 0.1*randn(n_lmod,1);
+%    if t>=0.0
+%       lmod_sig(1,k)=.1;
+%    elseif t>0.2
+%        lmod_sig(:,k)=zeros(n_lmod,1);
+%    end
+
+if n_lmod~=0 
+  if t<0.6
+     lmod_sig(1,k)= 0.5; 
+  else 
+%      lmod_sig(:,k) = zeros(n_lmod,1); 
+     lmod_sig(1,k) = 1; 
+  end 
+  
+%    if k >= 60 %t(k) >= t(70)
+%      lmod_sig(1,k)= -1.0 ; 
+%   end 
+end
